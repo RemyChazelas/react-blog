0856e52f8266f2c083042bc3e059206ff5c72803
Remy Chazelas <remychazelas@gmail.com>
2021-08-24 16:55:48 +0200

    add topbar component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Topbar from "./components/topbar/Topbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+    </Router>
+  );
+}
+
+export default App;
